12d6fc846b95b52406d3f4825690747ecb531fde
Tim Winfred <twinfred@gmail.com>
2023-05-11 01:00:43 -0600

    update max width of game board

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -5,7 +5,7 @@ export const GameBoard = () => {
   const { spaces } = useGame();
 
   return (
-    <div className="grid grid-cols-3 grid-rows-3 gap-1 sm: max-w-[40%] md:max-w-[35%] mx-auto">
+    <div className="grid grid-cols-3 grid-rows-3 gap-1 max-w-[75%] sm:max-w-[35%] mx-auto">
       {spaces.map((space, idx) => (
         <GameBoardSpace key={idx} index={idx}>
           {space}
